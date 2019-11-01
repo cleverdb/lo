26a6ca7c3791815af647212cef699d1089410246
zhangjihu <zhangjihu@chinamscp.com>
2019-11-01 17:30:51 +0800

    lalal

--- utils/urlConfig.js
+++ utils/urlConfig.js
@@ -4,6 +4,10 @@ let _apis = {
   "private_Courses": '/privateCourses',
   "private_Buy": '/privateBuy',
   "private_Buy_sel_teachers": '/privateBuy2',
+  "card_getCard": "/rest/s1/Goods/card/getCard",
+  "card_getCardDetail": (id) => `/rest/s1/Goods/card/getCardDetail?cardId=${id}`,
+  "card_sales": (id) => "/sales"
+
 }
 module.exports = {
   _apis
