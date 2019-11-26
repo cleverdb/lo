20ada1bb3ce7706cbeacfcbf33f17d1a553c1fc9
liushilei <shilei.liu@17zuoye.com>
2019-11-26 19:40:45 +0800

    update

--- pages/mineReport/mineReport.js
+++ pages/mineReport/mineReport.js
@@ -1,6 +1,20 @@
 // pages/mineReport/mineReport.js
 import Utils from '../../utils/util.js'
-const app = getApp()
+const app = getApp();
+const textPosition = {
+  one: {
+    left: 60,
+    top:180
+  },
+  two: {
+    left: 196,
+    top: 180
+  },
+  there: {
+    left: 356,
+    top: 180
+  }
+}
 Page({
   /**
    * 页面的初始数据
@@ -22,7 +36,9 @@ Page({
       2: '个月'
     },
     choose: 0,
-    isShow:false
+    isShow: false,
+    canWidth: '520',
+    canHeight:'830'
   },
   /**
    * 生命周期函数--监听页面加载
@@ -32,14 +48,24 @@ Page({
     const weakDat = Utils.plusDate(this.data.nowDate, -7);
     console.log(nowDate, weakDat);
     const ctx = wx.createCanvasContext('runCanvas');
+    const bigCanvas = wx.createCanvasContext('bigCanvas');
     wx.createSelectorQuery().select('#runCanvas').boundingClientRect(function (rect) { //监听canvas的宽高
       const w = parseInt(rect.width / 2); //获取canvas宽的的一半
       const h = parseInt(rect.height / 2); //获取canvas高的一半，
-      ctx.arc(w, h, w - 4, -0.5 * Math.PI, 1 * Math.PI);
+      ctx.beginPath();
+      ctx.setStrokeStyle('#727171');
+      ctx.setLineWidth("7"); //圆环的粗细
+      ctx.arc(w, h, w - 4, 0.7 * Math.PI, 2.3 * Math.PI);
+      ctx.stroke();
+      ctx.restore();
+
+      ctx.beginPath();
       ctx.setStrokeStyle("#FBC700"); //圆环线条的颜色
       ctx.setLineWidth("7"); //圆环的粗细
       ctx.setLineCap("round"); //圆环结束断点的样式 butt为平直边缘 round为圆形线帽 square为正方
+      ctx.arc(w, h, w - 4, 0.7 * Math.PI, 1 * Math.PI);
       ctx.stroke();
+      ctx.restore();
       //开始绘制百分比数字
       ctx.beginPath();
       ctx.setFillStyle("#FBC700"); // 字体颜色
@@ -69,7 +95,15 @@ Page({
      * 生命周期函数--监听页面显示
      */
   onReady: function () {
-    
+    const _this = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        const byclear = res.screenWidth / 375;
+        _this.setData({
+          byclear
+        })
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面隐藏
@@ -170,8 +204,42 @@ Page({
   },
   shareTap: function () {
     this.setData({
-      isShow:true
-    })
+      isShow: true
+    });
+  },
+  checkwh: function (e) {
+    console.log(e.detail);
+    const { byclear, canWidth, canHeight } = this.data;
+    const w = (canWidth / 2) * byclear;
+    const h = (canHeight / 2) * byclear;
+    const ctx = wx.createCanvasContext('myCanvas');
+    const textOneLeft = (textPosition.one.left / 2) * byclear;
+    const textTwoLeft = (textPosition.two.left / 2) * byclear;
+    const textThereLeft = (textPosition.there.left / 2) * byclear;
+    const textOneTop = (textPosition.one.top / 2) * byclear;
+    ctx.drawImage('/images/icon/log_min.png', 0, 0, w, h);
+    ctx.setFontSize(20 * byclear);
+    ctx.fillStyle = '#000';
+    ctx.fillText("190", textOneLeft, textOneTop);
+    ctx.fillText("1000", textTwoLeft, textOneTop);
+    ctx.fillText("2671", textThereLeft, textOneTop);
+    const txtWidth = ctx.measureText('1000').width;
+    const txtWidth2 = ctx.measureText('2671').width;
+    console.log(txtWidth);
+    ctx.setFontSize(15 * byclear);
+    ctx.fillText("min", ((textPosition.two.left + ((txtWidth*2)/byclear)) / 2) * byclear, textOneTop);
+    ctx.fillText("kcal", ((textPosition.there.left + ((txtWidth2 * 2) / byclear)) / 2) * byclear, textOneTop);
+    ctx.fillText("LIOU健身俱乐部", (152 / 2) * byclear, (800 / 2) * byclear);
+
+    ctx.setFontSize(10 * byclear);
+    ctx.fillText("月排名", textOneLeft, textOneTop + 20);
+    ctx.fillText("锻炼总时长", textTwoLeft, textOneTop + 20);
+    ctx.fillText("累计总消耗", textThereLeft, textOneTop + 20);
+    ctx.arc(35 * byclear, 35 * byclear, 25 * byclear, 0, 2 * Math.PI);
+    ctx.clip();
+    ctx.drawImage(app.globalData.wUserInfo.avatarUrl, 10 * byclear, 10 * byclear, 50 * byclear, 50 * byclear);
+    ctx.restore()
+    ctx.draw();
   },
   hideModalTap: function () {
     this.setData({
