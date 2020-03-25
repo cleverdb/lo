ded578766ab83c400a71869b18d13461b3d63b3a
grace zhang <gracezhang@gracedeMacBook-Air.local>
2020-03-25 19:35:58 +0800

    update

--- pages/publishCourse/publishCourse.js
+++ pages/publishCourse/publishCourse.js
@@ -5,7 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    current: "1",
+    index:0,
+    visible:false,
+    inputValue: null,
+    right: [{
+      text: '删除',
+      style: 'background-color: #E93560; color: white; font-size: 28rpx; width: 150rpx',
+    }],
+    banner: [
+      '/images/activity.png',
+      '/images/activity.png',
+      '/images/activity.png'
+    ],
+    activityIndex: -1,
+    classArr:[{
+        key:1,
+        value:'常规课',
+    },{
+        key: 2,
+        value: '拉伸课',
+    },{
+        key: 3,
+        value: '核心课',
+    },{
+        key: 4,
+        value: '恢复课',
+    },{
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }, {
+        key: 5,
+        value: '拳击课',
+      }],
+    array: ['美国', '中国', '巴西', '日本'],
   },
 
   /**
@@ -62,5 +113,44 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  onChange:function(e){
+    const { key } = e.detail;
+    this.setData({
+      current: key
+    })
+  },
+  classItemClick:function(e) {
+    const { item } = e.currentTarget.dataset;
+    const { key } = item;
+    this.setData({
+      activityIndex:key
+    })
+  },
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  inputChange: function(e){
+    const { value } = e.detail.detail;
+    this.setData({
+      inputValue: value
+    })
+  },
+  sumbit: function(){
+    this.setData({
+      visible:true
+    })
+  },
+  modalCancel: function(){
+    this.setData({
+      visible: false
+    })
+  },
+  modalOk: function() {
+    this.setData({
+      visible: false
+    })
   }
 })
