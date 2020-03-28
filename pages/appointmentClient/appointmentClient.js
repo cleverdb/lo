77899da1988858a72e49007e933d37ea216ba294
grace zhang <gracezhang@gracedeMacBook-Air.local>
2020-03-29 02:17:13 +0800

    update

--- pages/appointmentClient/appointmentClient.js
+++ pages/appointmentClient/appointmentClient.js
@@ -5,7 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    items: [
+      { name: 'old', value: '老会员' },
+      { name: 'new', value: '新客户'}
+      ],
+    storeArr :[{
+      key:1,
+      value:'LIOU健身乐城店'
+    },{
+      key: 2,
+      value: '健身乐城店'
+    },{
+      key: 3,
+      value: '乐城店'
+    }],
+    storeIndex: -1,
+    visible:false,
+    popContent:[{
+      key:1,
+      value:'内购'
+    }, {
+        key: 2,
+        value: '团购'
+      }, {
+        key: 3,
+        value: 'DM(外展)'
+      }, {
+        key: 4,
+        value: 'TI(电话咨询)'
+      }, {
+        key: 5,
+        value: 'DI(拉访)'
+      }, {
+        key: 6,
+        value: 'BR(会员介绍)'
+      }, {
+        key: 7,
+        value: 'APP'
+      },{
+        key: 8,
+        value: 'WI(访客)'
+      },{
+        key: 9,
+        value: '其他'
+      }],
+    popActive:{},
+    displayValue1:'请选择时间',
+    value1:[],
+    okShow:true
   },
 
   /**
@@ -62,5 +109,34 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  bindPickerChange:function(e){
+    const { value } = e.detail;
+    this.setData({
+      storeIndex: value
+    })
+  },
+  showNewMessage:function(){
+      this.setData({
+        visible: true
+      })
+  },
+  onClose:function(){
+    this.setData({
+      visible: false
+    })
+  },
+  popItemClick:function(e){
+    const { item } = e.currentTarget.dataset;
+    console.log('item', item);
+    this.setData({
+      popActive: item 
+    })
+  },
+  onOk:function(){
+    console.log('saassaas');
+    this.setData({
+      okShow:false
+    })
   }
 })
