b4cc859b7b09b144809107379dee2fd43910f389
grace zhang <gracezhang@gracedeMacBook-Air.local>
2020-03-28 18:37:54 +0800

    update

---
+++ pages/messageWarn/messageWarn.js
@@ -0,0 +1,69 @@
+// pages/messageWarn/messageWarn.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    right:[{
+      text: '删除',
+      style: 'background-color: #E73660; color: #ffffff; font-size:28rpx; width: 148rpx',
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
